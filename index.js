65c4104eec281994a1c4e694b31d7599dc1d8e7b
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-04-14 02:51:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-   var cartobj = new Object();
+   var cartobj = new Object((itemName : itemPrice));
    cartobj.itemName = item;
    cartobj.itemPrice = Math.floor(Math.random()*100);
    cart.push(item);
